8ab6d58e645e427bf8d817cb3d2e15e4c76ba15e
Prithwish Pramanik <prithwishpramanik462@gmail.com>
2021-09-08 14:30:44 +0530

    modification

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-$(document).ready(function(){
-    $(".stats").counterUp({
-        delay: 10,
-        time:1000
-    })
-})
+const counters = document.querySelectorAll('.stats');
+const speed = 200;
+
+counters.forEach((counter) => {
+    const updateCount = () => {
+        const target = parseInt(counter.getAttribute('data-target'));
+        const stats = parseInt(counter.innerText);
+        const increment = Math.trunc(target / speed);
+
+        if (stats < target) {
+            counter.innerText = stats + increment;
+            setTimeout(updateCount, 1);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    updateCount();
+});
+
